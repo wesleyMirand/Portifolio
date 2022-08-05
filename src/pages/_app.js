a02448845764d2a388765311185da1fefb57fb51
WesleyMirand <your_email_wewesantos@hotmail.com>
2022-08-04 21:03:23 -0300

    primeiro commit

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import '../../styles/globals.css'
+import Head from 'next/head'
+
+function MyApp ({ Component, pageProps}) {
+        return (
+        <>
+        <Head>
+            <title>Perfil | Vip</title>
+        </Head>
+        <Component {...pageProps} />
+        </>
+        )
+    }
+
+export default MyApp
