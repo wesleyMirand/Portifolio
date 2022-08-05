7ba4a05d94702ec5d7c11ddddf2e3611d8e4cfbb
WesleyMirand <your_email_wewesantos@hotmail.com>
2022-08-04 21:48:39 -0300

    primeiro commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../../styles/globals.css'
 import Head from 'next/head'
+import Header from '../../components/Header'
 
 function MyApp ({ Component, pageProps}) {
         return (
@@ -7,6 +8,7 @@ function MyApp ({ Component, pageProps}) {
         <Head>
             <title>Perfil | Vip</title>
         </Head>
+        <Header />
         <Component {...pageProps} />
         </>
         )
