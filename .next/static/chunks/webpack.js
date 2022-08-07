c44b57eb0fb1b15fa987496d29896bc662ce24f8
WesleyMirand <your_email_wewesantos@hotmail.com>
2022-08-06 22:55:01 -0300

    primeiro commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "273fc1cb2aa79d8d"; }
+/******/ 		__webpack_require__.h = function() { return "a0f89230fffede31"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { startTransition } = require('react/cjs/react.production.min')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,3 +9,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return 'my-build-id'
+  },
+}
